3fdbd609d38378cc00c5a46b80fc75f31397b359
johnny1204 <major@za2.so-net.ne.jp>
2021-01-31 22:54:13 +0900

    eslint導入

--- resources/js/components/TimeBlock.vue
+++ resources/js/components/TimeBlock.vue
@@ -1,14 +1,19 @@
 <template>
-  <div @click="modalOpen"></div>
+    <div @click="modalOpen"></div>
 </template>
 
 <script>
-  export default({
-    methods: {
-      modalOpen() {
-        this.$emit('open-modal', this.time)
-      }
+export default {
+    props: {
+        time: {
+            type: String,
+            default: null
+        }
     },
-    props: ['time']
-  });
+    methods: {
+        modalOpen() {
+            this.$emit('open-modal', this.time);
+        }
+    }
+};
 </script>
