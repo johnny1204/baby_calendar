7295b26983877f790b23096ce53f56cd966c0b46
johnny1204 <major@za2.so-net.ne.jp>
2021-06-25 15:52:54 +0900

    test

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -15,7 +15,7 @@ class HomeController extends Controller
     }
 
     /**
-     * Show the application dashboard.
+     * Show the application dashboard.test
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
