b9edd835a4e3266636cf0a869540b9524884b0f3
johnny1204 <major@za2.so-net.ne.jp>
2021-06-25 16:00:35 +0900

    test

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -11,17 +11,17 @@ class RedirectIfAuthenticated
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $reques
+     * @param  \Illuminate\Http\Request  $request
      * @param  \Closure  $next
      * @param  string|null  $guard
      * @return mixed
      */
-    public function handle($request1, Closure $next, $guard = null)
+    public function handle($request, Closure $next, $guard = null)
     {
         if (Auth::guard($guard)->check()) {
             return redirect(RouteServiceProvider::HOME);
         }
 
-        return $next($request1);
+        return $next($request);
     }
 }
